f4775557587780abe8097e5920aff8a325d2b9bc
Federico Bordón <105255410+federicobordon09-dev@users.noreply.github.com>
2025-04-23 18:28:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientId: '1349152461105008772', // Client ID del bot
   publicKey: '2e1280be1cc696ecdede6ddb68636d125b664125472f644bcbaea2b4935d67c7', // Public Key
-  channelId: '1364092777729753169', // ID del canal donde se enviarán las preguntas
+  channelId: '1364382772835385445', // ID del canal donde se enviarán las preguntas
 };
